dca186aabc21601816b9f6f0f8ce4193caa220cb
Pedro Féo <pheofo@gmail.com>
2020-05-31 14:43:13 -0300

    add user model and controller

---
+++ server/api/models/User.js
@@ -0,0 +1,22 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    cpf: {
+      type: "string",
+      required: true,
+      unique: true,
+      regex: /[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}/,
+    },
+    password: {
+      type: "string",
+      required: true,
+      encrypt: true,
+    },
+  },
+};
