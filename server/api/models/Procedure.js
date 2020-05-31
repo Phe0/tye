93f684d8227a71682fa20f2213fda2ed5c38631d
Pedro Féo <pheofo@gmail.com>
2020-05-31 14:50:17 -0300

    add procedure model and controller

---
+++ server/api/models/Procedure.js
@@ -0,0 +1,22 @@
+/**
+ * Procedure.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    number: {
+      type: "string",
+      required: true,
+      minLength: 20,
+      maxLength: 20,
+    },
+    cpf: {
+      type: "string",
+      required: true,
+      regex: /[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}/,
+    },
+  },
+};
