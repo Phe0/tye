30f0f953ced4aa283b3e4872fc9ab708ff758466
Pedro Féo <pheofo@gmail.com>
2020-06-03 14:56:06 -0300

    make newButton component

---
+++ client/src/components/NewButton/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PlusIcon from "../../assets/plusIcon";
+import { Link } from "react-router-dom";
+import "./style.scss";
+
+export default function SearchInput({ to }) {
+  return (
+    <Link to={to} style={{ textDecoration: "none" }}>
+      <button className="new-button bigger">
+        <PlusIcon color="#2b3831" />
+      </button>
+    </Link>
+  );
+}
--- client/src/pages/Procedure/index.js
+++ client/src/pages/Procedure/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SearchInput from "../../components/SearchInput";
+import NewButton from "../../components/NewButton";
 
 export default function Procedures() {
   return (
     <div>
       <SearchInput placeholder="Procure um processo..." />
+      <NewButton to="#" />
     </div>
   );
 }
