6892651648b1e0c96bc0d8e661d2c318a2ae1410
Pedro Féo <pheofo@gmail.com>
2020-06-03 13:59:47 -0300

    add ProcedureFolder component

---
+++ client/src/components/ProcedureFolder/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.scss";
+
+export default function ProcedureFolder({ number, cpf, id }) {
+  return (
+    <Link to="#" style={{ textDecoration: "none" }}>
+      <article className="folder">
+        <div className="folder__tag"></div>
+        <div className="folder__content">
+          <p>{number}</p>
+          <p>{cpf}</p>
+        </div>
+      </article>
+    </Link>
+  );
+}
--- client/src/pages/Procedure/index.js
+++ client/src/pages/Procedure/index.js
@@ -1,5 +1,14 @@
 import React from "react";
+import ProcedureFolder from "../../components/ProcedureFolder";
 
 export default function Procedures() {
-  return <div>aaaaaa</div>;
+  return (
+    <div>
+      <ProcedureFolder
+        id="1"
+        number="12345678901234567890"
+        cpf="053.332.081-07"
+      />
+    </div>
+  );
 }
