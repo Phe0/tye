42170f786a6beda6d050ded4f55b028cb532ebf7
Pedro Féo <pheofo@gmail.com>
2020-06-04 17:36:21 -0300

    add search to procedures page

--- client/src/components/SearchInput/index.js
+++ client/src/components/SearchInput/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import SearchIcon from "../../assets/searchIcon";
 import "./style.scss";
 
-export default function SearchInput({ placeholder }) {
+export default function SearchInput({ placeholder, value, onChange }) {
   return (
     <section className="search">
       <SearchIcon color="#2b3831" />
-      <input className="search__input" type="text" placeholder={placeholder} />
+      <input
+        className="search__input"
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
     </section>
   );
 }
--- client/src/pages/Procedures/index.js
+++ client/src/pages/Procedures/index.js
@@ -1,149 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import SearchInput from "../../components/SearchInput";
 import NewButton from "../../components/NewButton";
 import ProcedureFolder from "../../components/ProcedureFolder";
+import searchFunction from "../../utils/search";
 import "./style.scss";
 
-const procedures = [
+const proceduresTest = [
   {
     id: 1,
-    number: 12345678901234567890,
+    number: "11111111111111111111",
     cpf: "123.123.123-12",
   },
   {
     id: 1,
-    number: 12345678901234567890,
+    number: "12345678901234567890",
     cpf: "123.123.123-12",
   },
   {
     id: 1,
-    number: 12345678901234567890,
+    number: "12345678901234567890",
     cpf: "123.123.123-12",
   },
   {
     id: 1,
-    number: 12345678901234567890,
+    number: "12345678901234567890",
     cpf: "123.123.123-12",
   },
   {
     id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: 12345678901234567890,
+    number: "12345678901234567890",
     cpf: "123.123.123-12",
   },
 ];
 
 export default function Procedures() {
+  const [search, setSearch] = useState("");
+  const [procedures, setProcedures] = useState(proceduresTest);
+  const [filtered, setFiltered] = useState(proceduresTest);
+
+  function handleChange(event) {
+    setSearch(event.target.value);
+    const filteredProcedures = searchFunction(
+      procedures,
+      ["number", "cpf"],
+      event.target.value
+    );
+    setFiltered(filteredProcedures);
+  }
+
   return (
     <div className="container">
       <Header />
@@ -151,12 +56,16 @@ export default function Procedures() {
         <article className="intro">
           <h1>Processos</h1>
           <div className="intro__actions">
-            <SearchInput placeholder="Procure um processo" />
+            <SearchInput
+              value={search}
+              onChange={handleChange}
+              placeholder="Procure um processo"
+            />
             <NewButton to="/new-procedure" />
           </div>
         </article>
         <section className="folders">
-          {procedures.map((procedure, index) => (
+          {filtered.map((procedure, index) => (
             <ProcedureFolder
               key={index}
               id={procedure.id}
