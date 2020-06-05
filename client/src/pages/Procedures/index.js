07d6d9ab1894250d2f1bc63d3f87440da4afc94c
Pedro Féo <pheofo@gmail.com>
2020-06-05 13:33:35 -0300

    integrate Procedures page with backend

--- client/src/pages/Procedures/index.js
+++ client/src/pages/Procedures/index.js
@@ -1,43 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../components/Header";
 import SearchInput from "../../components/SearchInput";
 import NewButton from "../../components/NewButton";
 import ProcedureFolder from "../../components/ProcedureFolder";
 import searchFunction from "../../utils/search";
+import proceduresService from "../../services/Procedures";
 import "./style.scss";
-
-const proceduresTest = [
-  {
-    id: 1,
-    number: "11111111111111111111",
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: "12345678901234567890",
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: "12345678901234567890",
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: "12345678901234567890",
-    cpf: "123.123.123-12",
-  },
-  {
-    id: 1,
-    number: "12345678901234567890",
-    cpf: "123.123.123-12",
-  },
-];
 
 export default function Procedures() {
   const [search, setSearch] = useState("");
-  const [procedures, setProcedures] = useState(proceduresTest);
-  const [filtered, setFiltered] = useState(proceduresTest);
+  const [procedures, setProcedures] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+
+  useEffect(() => {
+    getProcedures();
+  }, []);
+
+  async function getProcedures() {
+    const proceduresArray = await proceduresService.getProcedures();
+    proceduresArray.sort((a, b) => {
+      if (a.createdAt > b.createdAt) return -1;
+      if (a.createdAt < b.createdAt) return 1;
+      return 0;
+    });
+    setProcedures(proceduresArray);
+    setFiltered(proceduresArray);
+  }
 
   function handleChange(event) {
     setSearch(event.target.value);
