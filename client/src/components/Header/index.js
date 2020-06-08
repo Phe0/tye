e4f2fa8c0f336d17b43a3058c321207e88a967a5
Pedro Féo <pheofo@gmail.com>
2020-06-08 12:52:31 -0300

    add logout button

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useHistory } from "react-router-dom";
 import "./style.scss";
 import isLawyer from "../../utils/isLawyer";
 
 export default function Header() {
-  let location = useLocation();
+  const location = useLocation();
+  const history = useHistory();
+
+  function logout() {
+    localStorage.removeItem("accessToken");
+    history.push("/");
+  }
+
   return (
     <header className="header">
       <img src={require("../../assets/Logo.png")} alt="logo" />
-      {isLawyer() && (
-        <nav className="navigation">
+
+      <nav className="navigation">
+        {isLawyer() && (
           <Link to="/rules" style={{ textDecoration: "none" }}>
             <button
               className={`navigation__button ${
@@ -19,6 +27,8 @@ export default function Header() {
               Regras
             </button>
           </Link>
+        )}
+        {isLawyer() && (
           <Link to="/procedures" style={{ textDecoration: "none" }}>
             <button
               className={`navigation__button ${
@@ -28,8 +38,16 @@ export default function Header() {
               Processos
             </button>
           </Link>
-        </nav>
-      )}
+        )}
+        {localStorage.getItem("accessToken") && (
+          <button
+            onClick={() => logout()}
+            className="navigation__button bigger"
+          >
+            Sair
+          </button>
+        )}
+      </nav>
     </header>
   );
 }
